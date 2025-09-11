457a4d3a563475ee85c816df6462b2c4b8d165c2
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-11 15:18:23 +0500

    Task1

---
+++ Tyuiu.ReshetovaAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Арина";
+        int age = 17;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет");
+        Console.ReadKey();
+    }
+}
