afbfd1af7a159665a75e89c2a96e5e7dee4ef6b7
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-11 20:36:19 +0500

    Task2

---
+++ Tyuiu.ReshetovaAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ReshetovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ReshetovaAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Арина";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Арина", res);
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using Tyuiu.ReshetovaAA.Sprint0.Task2.V0.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Арина"));
+        Console.ReadKey();
+    }
+}
