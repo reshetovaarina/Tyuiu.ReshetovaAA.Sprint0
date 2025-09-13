706f8bac69edc5cecb11703b16c76857871a53bd
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-13 19:29:47 +0500

    Task4

---
+++ Tyuiu.ReshetovaAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.ReshetovaAA.Sprint0.Task4.V0.Lib;
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(15, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
+
