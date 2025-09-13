8e9136e2f08b0ad48b6f2c839ec1aaf061bc584d
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-13 21:35:59 +0500

    Task6

---
+++ Tyuiu.ReshetovaAA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.ReshetovaAA.Sprint0.Task6.V0.Lib;
+int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine("Сумма элементов массив = " + DataService.AdditionArray(arraynums));
+Console.WriteLine("Разность элемнтов массив = " + DataService.SubtractionArray(arraynums));
+Console.WriteLine("Произведение элемнтов массива = " + DataService.MultiplicationArray(arraynums));
+Console.ReadKey();
